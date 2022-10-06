215747147283cf5d6ac08487354aba530ff38e9b
MEHDI-svg <mehdiblabo@gmail.com>
2022-10-06 20:51:19 +0100

    useful of statefulwidget setstate

--- lib/Lessontwo.dart
+++ lib/Lessontwo.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 
 class LessonTwo extends StatefulWidget {
+  const LessonTwo({Key? key}) : super(key: key);
+
   @override
-  State<StatefulWidget> createState() => LessonTwoState();
+  State<StatefulWidget> createState() {
+    return LessonTwoState();
+  }
 }
 
-class LessonTwoState extends State {
+class LessonTwoState extends State<LessonTwo> {
+  var c = Colors.orange;
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: c,
+      ),
+      body: null,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            c = Colors.red;
+          });
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/Lessontwo.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,80 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            leading:
-                const Icon(Icons.add_reaction_outlined, color: Colors.black),
-            title: const Text("Hello world"),
-            actions: [
-              ElevatedButton.icon(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.black),
-                ),
-
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.login,
-                  size: 24.0,
-                ),
-                label: const Text('login'), // <-- Text
-              ),
-            ],
-          ),
-          body: Container(
-              alignment: Alignment.center,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton(onPressed: () {}, child: const Text("A")),
-                      OutlinedButton(
-                          onPressed: () => print("hello word"),
-                          child: const Text("B")),
-                      TextButton.icon(
-                          onPressed: () {},
-                          icon: const Icon(Icons.access_alarm_rounded),
-                          label: const Text("Alarm"))
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton(onPressed: () {}, child: const Text("A")),
-                      OutlinedButton(
-                          onPressed: () => print("hello word"),
-                          child: const Text("B")),
-                      TextButton.icon(
-                          style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(Colors.black),
-                            mouseCursor:
-                                MaterialStateProperty.all(MouseCursor.defer),
-                            padding: MaterialStateProperty.all(
-                                const EdgeInsets.all(10)),
-                          ),
-                          onPressed: () {},
-                          icon: const Icon(Icons.access_alarm_rounded),
-                          label: const Text("Alarm"))
-                    ],
-                  )
-                ],
-              )),
-          floatingActionButton: FloatingActionButton.extended(
-            label: const Text("ADD"),
-            icon: const Icon(Icons.add),
-            onPressed: () {},
-            backgroundColor: const Color(0xFF4A148C),
-            foregroundColor: Colors.red,
-            shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(30)),
-                side: BorderSide(color: Colors.red, width: 6)),
-            splashColor: Colors.yellow,
-          ),
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerFloat,
-        ));
+        theme: ThemeData(primarySwatch: Colors.teal),
+        home: const LessonTwo());
   }
 }
